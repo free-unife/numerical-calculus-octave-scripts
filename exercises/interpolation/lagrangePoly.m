32778c73ff80cdb1fce25f49a2ebbeb672cad6e8
frnmst/Franco Masotti <franco.masotti@live.com>
2015-12-08 13:06:51 +0100

    Added lagrangePolySimpler.m. Added comments to lagrangePoly.m which still needs to be corrected.

--- exercises/interpolation/lagrangePoly.m
+++ exercises/interpolation/lagrangePoly.m
@@ -20,17 +20,35 @@ p = zeros (size (evalPoints));
 % X is the matrix of the Ls (plural L).
 for i = 1 : n1
 	% Skip step when i == k.
-	a (i) = y (i) / prod (x (i) - x ([1 : i - 1, i + 1 : n1]));
+	%
+	%
+	% WARNING
+	% SOMETHING IS WRONG HERE!!!
+	a(i) = y(i)/prod (x(i)-x([1:i-1,i+1:n1]));
+	%
+	%
 end;
 
 % Calculate the polynomial value with each evaluation point.
 for k = 1 : length (evalPoints)
+	% Check if the value of f(x) in evalPoint must be found or has already
+	% been given in input with x(index), y(index).
 	ij = find (evalPoints (k) == x);
 	if isempty (ij)
+		% Find the numerator of the Lagrange formula.
+		% Using mathematical notation:
+		% temp = (evalPoint - x0) * (evalPoint - x1) * ... * (evalPoint - xn)
 		temp = prod (evalPoints (k) - x);
+		% Find the denominator.
+		% temp * (a0                 ...   an       )
+		%        (---------------- + ... + ---------)
+		%        ((evalPoint - x0)   ...   (.. - xn))
 		p (k) = temp * sum (a ./ (evalPoints (k) - x));
 	else
-		p (k) = y (ij(1));
+		% If the current evalPoint is the same as one of the x points,
+		% then we already know the value of that y since it has been
+		% given in input.
+		p (k) = y (ij (1));
 	end;
 end;
 
---
+++ exercises/interpolation/lagrangePolySimpler.m
@@ -0,0 +1,48 @@
+% Copyright © 2015 Franco Masotti <franco.masotti@student.unife.it>
+% This work is free. You can redistribute it and/or modify it under the
+% terms of the Do What The Fuck You Want To Public License, Version 2,
+% as published by Sam Hocevar. See the LICENSE file for more details.
+
+
+function [p] = lagrangePolySimpler (x, y, evalPoints);
+%  [p] = lagrangePolySimpler (x, y, evalPoints);
+%
+% Calculate Lagrange polynomials using Lagrange bases.
+%
+% I = { array x = observation points, array y = observed data at
+% observation points x, array evalPoints represents the set of points where
+% to evaluate the polynomial. }
+% P = { x, y and evalPoins must be 1xn, 1xn and 1xm arrays respectively. }
+% O = { p is the array containing the value of the polynomial for each
+% evalPoint. }
+% C = { p is 1x(length (evalPoints) and p = sum (sum (L (i) * y (i))) }
+
+
+n = length (x);
+p = zeros (size (evalPoints));
+%a = ones (size (evalPoints));
+
+% Get the coefficients.
+% Look at lagrangePoly.m for more info.
+%for i = 1 : length (y)
+        % Skip step when i == k.
+%	a (i) = y (i) / prod (x (i) - x ([1 : i - 1, i + 1 : length(y)]));
+%end;
+
+for k = 1 : n
+	% lagrangeBase is the array of the sum of all the lagrange bases (for
+	% every evalPoint.
+	lagrangeBase = ones (size (evalPoints));
+        % Skip step when i == k, infact:
+        % Lk (xi) = (evalPoints - xk) / (xk - xi)
+        % where i must not be equal to k (otherwise the result
+        % would be Inf).
+        % lagrangeBase is similar to the one in the definition. The only
+        % difference here is that it's already evaluated in x = x (i).
+	for i = [1 : k - 1, k + 1 : n]
+		lagrangeBase = ((evalPoints - x (i)) ./ (x(k) - x(i))) .* lagrangeBase;
+	end;
+	% Calculatate the value of the polynom.
+	% pn (x) = y0 * L0(x) + y1 * L1 (x) + ... yn * Ln (x).
+	p = p + (lagrangeBase * y (k))
+end;
