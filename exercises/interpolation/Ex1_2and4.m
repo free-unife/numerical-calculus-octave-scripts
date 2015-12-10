0c4d7cfb3b1736ec2b0d37488f26adcdb724ef6f
frnmst/Franco Masotti <franco.masotti@live.com>
2015-12-10 18:39:57 +0100

    Corrected exercises.

--- exercises/interpolation/Ex1_2and4.m
+++ exercises/interpolation/Ex1_2and4.m
@@ -11,40 +11,29 @@ func = @(x) x + e .^ (1 ./ (1 + x.^2));
 fplot (func, [-2 2], 'b');
 
 % Lagrange polynom has degree 6.
-observationX = [-2 : 0.5 : 2];
-observationY = zeros (size (observationX));
-x = observationX;
-y = observationY;
+observationX = linspace (-2, 2, 7);
+observationY = observationX + e .^ (1 ./ (1 + observationX.^2));
 
-% Calculate value of function in observation points.
-y = x + e .^ (1 ./ (1 + x.^2));
+evalPoints = [-2: 0.1: 2];
+[y1] = lagrangePoly (observationX, observationY, evalPoints);
+plot (evalPoints, y1, 'r');
 
-% Get evaluation points in the lineary spaced interval between -2 and 2.
-evalPointsL = linspace (-2, 2, 6);
-[yL1] = polyVandermonde (x, y, evalPointsL);
-plot (evalPointsL, yL1, 'r');
 
-% Lagrange function with Chebishev nodes (degree 6).
-evalPointsC = chebyshevNodes (-2, 2, 6)
-%evalPoints = chebTest (6, 'Tn', [-2 2])
-[yL2] = polyVandermonde (x, y, evalPointsC);
-plot (evalPointsC, yL2, 'g');
+chebNodes = chebyshevNodes (-2, 2, 7)
+chebY =  chebNodes + e .^ (1 ./ (1 + chebNodes.^2))
+[y2] = lagrangePoly (chebNodes, chebY, evalPoints);
+plot (evalPoints, y2, 'g');
 
-hold off
 
-% Create a new graphics window.
 figure
-
 hold on
 
-% Calculate and plot errors for both cases.
-y = evalPointsL + e .^ (1 ./ (1 + evalPointsL.^2));
-El1 = abs (y - yL1)
-plot (0 : 5, El1, 'g');
 
-y = evalPointsC + e .^ (1 ./ (1 + evalPointsC.^2));
-El2 = abs (y - yL2)
-plot (0 : 5, El2, 'b');
+yNew = evalPoints + e .^ (1 ./ (1 + evalPoints.^2));
+Ea = abs (yNew - y1)
+Eb = abs (yNew - y2)
 
+plot (evalPoints, Ea, 'b');
+plot (evalPoints, Eb, 'r');
 
 hold off
--- exercises/interpolation/chebyshevNodes.m
+++ exercises/interpolation/chebyshevNodes.m
@@ -4,7 +4,7 @@ function [x] = chebyshevNodes (a, b, n);
 % Find Chebyshev polynomial with the direct formula and iteratively. TODO 
 % coefficient output.
 %
-% I = { a = min, b = max, n = degree. }
+% I = { a = min, b = max, n = node number = degree + 1. }
 % P = {}
 % O = {}
 % C = {}
@@ -14,7 +14,6 @@ x = zeros (1, n);
 for k = 1 : n
 	first = (1 / 2) * (a + b);
 	second = (1 / 2) * (b - a);
-	third = cos ((((2 * k) - 1) / (2 * n)) * pi);
-%	third = cos (((2 * k) / (2 * n)) * pi);
-	x (k) = first + (second * third);
+	third = cos (((2 * k + 1) * pi) / (2 * (n + 1)));
+	x (k) = (second * third) + first;
 end;
